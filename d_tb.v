d8a115663f506742a35bf7293b03be1809920f12
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-05-13 16:45:00 +0530

    Create d_tb.v

---
+++ d_tb.v
@@ -0,0 +1,22 @@
+module d_tb();
+reg D;
+reg clk;
+wire Q;
+wire Qbar;
+
+d dut(D,clk,Q,Qbar);
+
+initial begin
+  clk=0;
+  forever #10 clk = ~clk;  
+end 
+initial begin 
+ D = 0;
+ #100;
+ D = 1;
+ #100;
+ D = 0;
+ #100;
+ D = 1;
+end 
+endmodule 
