5b1d1065063820559fb3e5ad55da5c4062b565cf
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-05-15 12:32:05 +0530

    Create tff_tb.v

---
+++ tff_tb.v
@@ -0,0 +1,38 @@
+module t_flip_tb;
+
+	// Inputs
+	reg t;
+	reg clk;
+	reg reset;
+
+	// Outputs
+	wire q;
+
+	// Instantiate the Unit Under Test (UUT)
+	t_flip uut (
+		.t(t), 
+		.clk(clk), 
+		.reset(reset), 
+		.q(q)
+	);
+
+	initial begin
+		// Initialize Inputs
+		t = 0;
+		clk = 1;
+		reset = 1;	
+		t = 1;
+		reset = 0;
+		#10;
+		t=0;
+		#10;
+		t=1;
+		#20;
+		t=0;
+		#50;
+		t=1;
+	end
+	always #10 clk=~clk;
+      
+endmodule
+
