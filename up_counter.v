73c1188aaf976e54fe927279bd394e0e7093dd99
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2023-05-15 12:07:08 +0530

    Create up_counter.v

---
+++ up_counter.v
@@ -0,0 +1,11 @@
+module counter(clk,count_out);
+	 input clk;
+	 output reg [3:0]count_out;
+	 initial begin
+	 count_out=4'b0;
+	 end
+	 always@(posedge clk)
+	 begin
+	 count_out<=count_out+1'b1;
+end
+endmodule
